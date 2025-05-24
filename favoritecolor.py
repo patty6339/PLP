5ea74e59d9e48414916434cfa4894fa4bb4649b7
Patrick Odhiambo <patty6339@gmail.com>
2025-05-24 08:45:59 +0300

    Create favoritecolor.py
    A program that asks user for name and favorite color as inputs and prints the output

---
+++ favoritecolor.py
@@ -0,0 +1,8 @@
+# Ask for user's name
+name = input("What's your name? ")
+
+# Ask for favorite color
+color = input("What's your favorite color? ")
+
+# Create and print the greeting
+print(f"Hello, {name}! Your favorite color, {color}, is awesome!")
